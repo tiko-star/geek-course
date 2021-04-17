653e128d0cef856c2a3d7c2761f72e236c0dc48c
Mariam <marhakobyan22@gmail.com>
2021-04-18 00:41:59 +0400

    homework1

--- homework/mariam_hakobyan/arajadranq_1.php
+++ homework/mariam_hakobyan/arajadranq_1.php
@@ -13,27 +13,3 @@ echo var_dump($b).var_export($b)."\n";
 echo var_dump($female).var_export($female)."\n";
 
 
-echo("Arajadranq 2"."\n");
-echo is_float($a)."\n";
-echo var_dump(is_numeric($age))."\n";
-echo is_bool($female)."\n";
-echo is_int($a)."\n" ;
-echo var_dump(is_int($name))."\n";
-echo is_null($b)."\n";
-echo gettype($a)."\n";
-echo gettype($name)."\n";
-
-
-echo("Arajadranq 3"."\n");
-*if (isset($age)){
-	echo var_export("age"); 
-}
-unset($a);
-echo var_dump(empty($a));
-if (isset($name)==true){
-	echo "name-> ";
-echo var_export($name)."\n" ;
-}
-else{
-    echo "no name"."\n";
-}
