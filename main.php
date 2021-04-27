ae8b2bf24be93d43074b295a92dbed0c361005b2
Alik77-bit <80385731+Alik77-bit@users.noreply.github.com>
2021-04-27 18:39:37 +0400

    homework

---
+++ main.php
@@ -0,0 +1,28 @@
+<?php
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+$name = "Alik";
+$number = 7;
+$number_2 = 7.77;
+$boolean = True;
+$boolean_2 = False;
+
+
+var_dump($name) 
+var_dump($number);
+var_dump($number_2);
+var_dump($boolean);
+var_dump($boolean_2);
+
+var_export($number);
+var_export($number_2);
+var_export($name);
+var_export($boolean);
+var_export($boolean_2);
